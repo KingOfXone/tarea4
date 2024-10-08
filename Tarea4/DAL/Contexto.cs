55416d61149e2e8a805cd8ef8ed29643a2fe4398
Miguel Ángel Betances Beato <betancesbeato@gmail.com>
2024-10-07 23:37:58 -0400

    Agregando Contexto

---
+++ Tarea4/DAL/Contexto.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Tarea4Lib.Models;
+
+namespace Tarea4.DAL
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+        public DbSet<Tickets> Tickets { get; set; }
+        public DbSet<Prioridades> Prioridades { get; set; }
+        public DbSet<Clientes> Clientes { get; set; }
+        public DbSet<Sistemas> Sistemas { get; set; }
+     
+    }
+}
